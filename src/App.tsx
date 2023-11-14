3cfc26700e807f3b68b2b3f152aacf525fac862b
Jhorranis <jhorranis@gmail.com>
2023-11-14 13:14:02 -0300

    feat(task): list and toggle complete tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
 import "./global.css";
 
 import styles from "./App.module.css";
@@ -9,15 +11,52 @@ import { Input } from "./componets/Input";
 import { PlusCircle } from "@phosphor-icons/react";
 
 import { Empty } from "./componets/Empty";
-import { Task } from "./componets/Task";
+import { Task, TaskItem } from "./componets/Task";
 
 function App() {
+  const [tasks, setTasks] = useState<TaskItem[]>([]);
+  const [newTaskText, setNewTaskText] = useState("");
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value);
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    setTasks((state) => [
+      ...state,
+      {
+        checked: false,
+        text: newTaskText,
+      },
+    ]);
+  }
+
+  function completeTask(currentTask: TaskItem) {
+    const tasksToggled = tasks.map((task) => {
+      if (task.text === currentTask.text) {
+        task.checked = !task.checked;
+      }
+
+      return task;
+    });
+
+    setTasks(tasksToggled);
+  }
+
   return (
     <div>
       <Header />
       <main className={styles.content}>
-        <form>
-          <Input placeholder="Adicione uma nova tarefa" />
+        <form onSubmit={handleCreateNewTask}>
+          <Input
+            placeholder="Adicione uma nova tarefa"
+            value={newTaskText}
+            onChange={handleNewTaskChange}
+            // onInvalid={handleNewTaskInvalid}
+            required
+          />
           <Button type="submit">
             Criar <PlusCircle size={16} />
           </Button>
@@ -27,18 +66,23 @@ function App() {
           <header className={styles.tasksHeader}>
             <div className={`${styles.tasksCreated} ${styles.tasksInfo}`}>
               Tarefas Criadas
-              <span className={styles.tasksCounter}>0</span>
+              <span className={styles.tasksCounter}>{tasks.length}</span>
             </div>
 
             <div className={`${styles.tasksDone} ${styles.tasksInfo}`}>
               Concluídas
-              <span className={styles.tasksCounter}>0</span>
+              <span className={styles.tasksCounter}>
+                {tasks.filter((task) => !!task.checked).length} de{" "}
+                {tasks.length}
+              </span>
             </div>
           </header>
 
-          <Empty />
-          <Task checked />
-          <Task checked={false} />
+          {tasks.length === 0 && <Empty />}
+          {tasks.length > 0 &&
+            tasks.map((task) => (
+              <Task task={task} onCompleteTask={completeTask} />
+            ))}
         </section>
       </main>
     </div>
--- src/componets/Task.tsx
+++ src/componets/Task.tsx
@@ -2,23 +2,33 @@ import styles from "./Task.module.css";
 
 import { Trash, Check } from "@phosphor-icons/react";
 
-interface TaskProps {
+export interface TaskItem {
   checked: boolean;
+  text: string;
 }
 
-export function Task({ checked = false }: TaskProps) {
+interface TaskProps {
+  task: TaskItem;
+  onCompleteTask: (task: TaskItem) => void;
+}
+
+export function Task({ task, onCompleteTask }: TaskProps) {
+  function handleCompleteTask() {
+    onCompleteTask(task);
+  }
+
   return (
     <div className={styles.task}>
       <div className={styles.inputIconWrapper}>
-        <div className={`${styles.input} ${checked && styles.inputDone}`}>
-          {checked && <Check size={10} className={styles.inputIcon} />}
+        <div
+          className={`${styles.input} ${task.checked && styles.inputDone}`}
+          onClick={handleCompleteTask}
+        >
+          {task.checked && <Check size={10} className={styles.inputIcon} />}
         </div>
       </div>
 
-      <p className={checked ? styles.underline : ""}>
-        Integer urna interdum massa libero auctor neque turpis turpis semper.
-        Duis vel sed fames integer.
-      </p>
+      <p className={task.checked ? styles.underline : ""}>{task.text}</p>
 
       <button className={styles.deleteIconWrapper}>
         <Trash size={14} className={styles.deleteIcon} />
